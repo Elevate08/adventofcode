0c61389efe4fd296a694fa38af56c530cec82a74
elevate <3460250+Elevate08@users.noreply.github.com>
2020-12-03 21:45:26 -0500

    day 2 is completed, 4 stars total

---
+++ day_2/main.py
@@ -0,0 +1,39 @@
+# Source
+
+import re
+
+def validate(data):
+    # Character must be in the password a min and no more than max amount of times
+    step_one_valid = 0
+    step_two_valid = 0
+    for line in data:
+        line = re.compile("(-|:*\s)").split(line)
+        min = int(line[0])
+        max = int(line[2])
+        char = line[4]
+        password = line[6]
+    
+        if password.count(char) in range(min, max+1):
+            step_one_valid += 1
+
+        match = 0
+        for index in [min-1, max-1]:
+            try:
+                if char == password[index]:
+                    match += 1
+            except Exception as e:
+                print(len(password))
+                
+        if match == 1:
+            step_two_valid += 1
+
+    return step_one_valid, step_two_valid
+
+
+def main():
+    with open('input') as data:
+        print(validate(data))
+        
+
+if __name__ == "__main__":
+    main()
