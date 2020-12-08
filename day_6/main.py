921937f0a9f75d33f55637d95ba411839f9fb12a
elevate <3460250+Elevate08@users.noreply.github.com>
2020-12-07 22:28:05 -0500

    day 6 complete, learned about creating copies of lists when removing values during for loop

--- day_6/main.py
+++ day_6/main.py
@@ -1,37 +1,91 @@
+class Group():
+    def __init__(self):
+        self.members = []
+
+    def new_member(self, data):
+        self.members.append(data)
+
+    def valid_answers(self):
+        for i, member in enumerate(self.members):
+            if i == 0:
+                source = sorted(list.copy(member))
+                valid = list.copy(source)
+            else:
+                for letter in source:
+                    if letter not in member:
+                        try:
+                            valid.remove(letter)
+                        except Exception:
+                            pass
+
+        return valid
+
+
+def getSeparated(data):
+    result = []
+    valid = []
+    for row in data:
+        try:
+            for char in row:
+                valid.append(char)
+        except Exception:
+            valid.append(row)
+
+        if row == "\n\n":
+            result.append(valid)
+            valid = []
+
+    return result
+
+
 # Step 1, all unique characters
-def yesFinder(data):
-    unique = []
-    steptwo = []
-    result_1 = 0
-    result_2 = 0
+def stepOne(data):
+    valid = []
+    result = 0
     for line in data:
         if len(line) == 1:
-            result_1 += len(unique)
-            result_2 += len(steptwo)
-            unique = []
-            steptwo = []
+            result += len(valid)
+            valid = []
         else:
             for char in line[:-1]:
-                if char not in unique:
-                    unique.append(char)
+                if char not in valid:
+                    valid.append(char)
 
-            if len(steptwo) == 0:
-                steptwo.append(line)
-            else:
-                for char in steptwo:
-                    if char not in line:
-                        steptwo.remove(char)
+    return result + len(valid)
 
-    return result_1 + len(unique), result_2 + len(steptwo)
+
+def stepTwo(data):
+    result = []
+    group = Group()
+    count = 0
+    for line in data:
+        if len(line) != 1:
+            answers = []
+            for character in line[:-1]:
+                answers.append(character)
+            group.new_member(answers)
+        else:
+            if '\n' in line:
+                result.append(group)
+                group = Group()
+
+    result.append(group)
+
+    for group in result:
+        valid = group.valid_answers()
+        count += len(valid)
+
+    return count
 
 
 def main():
-    # filename = 'input'
-    filename = 'test'
-    result = 0
+    filename = 'input'
+    # filename = 'testing'
+
     with open(filename) as data:
-        result = yesFinder(data)
-    print(result)
+        file = data.readlines()
+        print(stepOne(file))
+        print(stepTwo(file))
 
 
 if __name__ == "__main__":
